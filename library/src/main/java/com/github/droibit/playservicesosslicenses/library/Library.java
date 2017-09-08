83b19187f9efd8701e4ae21de34637db1c568006
Shinya Kumagai <roomful.rooms@gmail.com>
2017-09-08 13:47:54 +0900

    Initial Commit.

---
+++ library/src/main/java/com/github/droibit/playservicesosslicenses/library/Library.java
@@ -0,0 +1,12 @@
+package com.github.droibit.playservicesosslicenses.library;
+
+
+import android.support.annotation.NonNull;
+
+public class Library {
+
+    @NonNull
+    public String sayHello() {
+        return "Hello!!";
+    }
+}
