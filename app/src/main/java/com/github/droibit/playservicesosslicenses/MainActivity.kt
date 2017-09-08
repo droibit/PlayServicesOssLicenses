32f34f7a6fe7969029c0d6304b1cf8f64bc307f4
Shinya Kumagai <roomful.rooms@gmail.com>
2017-09-08 14:14:02 +0900

    Implement show oss licenses

--- app/src/main/java/com/github/droibit/playservicesosslicenses/MainActivity.kt
+++ app/src/main/java/com/github/droibit/playservicesosslicenses/MainActivity.kt
@@ -2,11 +2,20 @@ package com.github.droibit.playservicesosslicenses
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import android.content.Intent
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showButton.setOnClickListener {
+            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
+        }
     }
 }
