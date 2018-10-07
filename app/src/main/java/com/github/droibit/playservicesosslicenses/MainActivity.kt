685592a2c70d0a436b47195ae39825b1b88f1708
Shinya Kumagai <roomful.rooms@gmail.com>
2018-10-08 00:46:13 +0900

    Update all dependencies

--- app/src/main/java/com/github/droibit/playservicesosslicenses/MainActivity.kt
+++ app/src/main/java/com/github/droibit/playservicesosslicenses/MainActivity.kt
@@ -1,21 +1,19 @@
 package com.github.droibit.playservicesosslicenses
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import android.content.Intent
-
-
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+import kotlinx.android.synthetic.main.activity_main.showButton
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-        showButton.setOnClickListener {
-            startActivity(Intent(this, OssLicensesMenuActivity::class.java))
-        }
+    showButton.setOnClickListener {
+      startActivity(Intent(this, OssLicensesMenuActivity::class.java))
     }
+  }
 }
